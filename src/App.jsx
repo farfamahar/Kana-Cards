591302c2d3c08a6bed2f499869d8467ac12267ef
Farfama <farfama.hargaaya@gmail.com>
2022-11-13 22:22:34 +0900

    Update dakuten and handakuon, code cleanup
    -dakuten and handakuon added to quiz options
    -Code organized with proper indentation

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,16 @@ function App() {
 
   const [quiz, setQuiz] = useState('');
   const [show, setShow] = useState(true);
-  const [difficulty, setDifficulty] = useState(3);
+  const [difficulty, setDifficulty] = useState(10);
+  const [isDakutan, setIsDakutan] = useState(false);
 
-  function onChangeValue(event) {
+
+  function handleDifficulty(event) {
     setDifficulty(event.target.value);
-    // console.log(event.target.value);
-    console.log(difficulty)
+  }
+
+  function handleDakutan(event) {
+    setIsDakutan(event.target.checked);
   }
 
 
@@ -50,24 +54,24 @@ function App() {
         <hr className='mt-3 mb-3'/>
 
         <h1 className='text-1xl font-bold uppercase mt-5 mb-3 text-black	' > Quiz</h1>
-        <div class="flex" onChange={onChangeValue}>
+        <div class="flex" onChange={handleDifficulty}>
           <div class="flex items-center mr-4">
-            <input  id="inline-radio" type="radio" value={10} name="inline-radio-group" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800  dark:bg-gray-700 dark:border-gray-600"/>
+            <input  checked={difficulty == 10} id="inline-radio" type="radio" value={10} name="inline-radio-group" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800  dark:bg-gray-700 dark:border-gray-600"/>
             <label for="inline-radio" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Easy</label>
           </div>
           <div class="flex items-center mr-4">
-            <input id="inline-2-radio" type="radio" value={6} name="inline-radio-group" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300  dark:ring-offset-gray-800  dark:bg-gray-700 dark:border-gray-600"/>
+            <input checked={difficulty == 6} id="inline-2-radio" type="radio" value={6} name="inline-radio-group" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300  dark:ring-offset-gray-800  dark:bg-gray-700 dark:border-gray-600"/>
             <label for="inline-2-radio" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Medium</label>
           </div>
           <div class="flex items-center mr-4">
-            <input id="inline-3-radio" type="radio" value={3} name="inline-radio-group" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600"/>
+            <input checked={difficulty == 3} id="inline-3-radio" type="radio" value={3} name="inline-radio-group" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600"/>
             <label for="inline-3-radio" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Hard</label>
           </div>
         </div>
 
         <div class="flex items-center mb-4 mt-4">
-          <input id="default-checkbox" type="checkbox" value="" className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800  dark:bg-gray-700 dark:border-gray-600"/>
-          <label for="default-checkbox" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Include Rendaku</label>
+          <input id="default-checkbox" type="checkbox" onChange={handleDakutan} value="" className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800  dark:bg-gray-700 dark:border-gray-600"/>
+          <label for="default-checkbox" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Include dakuten and handakuon</label>
         </div>
         <div className='mb-5 '>
           <button className="transition ease-in-out delay-100 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-200 ... bg-blue-500 text-white font-bold py-2 px-4 rounded" onClick={handleHiraganaQuizChoice}> Hiragana</button>
@@ -75,7 +79,7 @@ function App() {
         </div>
       </div>
     </div>}
-      {quiz && <KanaQuiz quiz={quiz} difficulty={difficulty} stateChanger={setShow}/>}
+      {quiz && <KanaQuiz quiz={quiz} difficulty={difficulty} dakutan={isDakutan} stateChanger={setShow}/>}
       <div className='bg-scroll'> </div>
     </div> 
     )
