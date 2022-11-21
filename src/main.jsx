001abaa982f22056d067f6ec5ed690a2a32fd4a7
Farfama <farfama.hargaaya@gmail.com>
2022-11-22 05:12:54 +0900

    Update CSS organization
    -Broke up css into multiple files
    -Removed unused images

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import './card.css'
+import './shake.css'
+import './background.css'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
