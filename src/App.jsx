9ff45bf3d26e98403fac6d698383c7c879d826df
Farfama <farfama.hargaaya@gmail.com>
2022-11-23 23:51:24 +0900

    Update tooltip on hover
    Tooltip added on hover of difficulty buttons

--- src/App.jsx
+++ src/App.jsx
@@ -51,6 +51,8 @@ function App() {
 
   }
   
+  //TODO: Simplify tailwind css
+  //TODO: Add tooltips to settings (hover to see what each one do)
   return (
     <div>
     {show &&
@@ -68,6 +70,13 @@ function App() {
           difficulty={difficulty}
         />
 
+        {/* <a href="#" class="group relative inline-block text-blue-500 underline hover:text-red-500 duration-300">
+            Link with top tooltip
+            <span
+                class="absolute hidden group-hover:flex -left-5 -top-2 -translate-y-full w-48 px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700">This
+                is some extra useful information</span>
+        </a> */}
+        
         <ChooseDakutan
           handleDakutan={handleDakutan}
         />
--- src/components/ChooseDifficulty.jsx
+++ src/components/ChooseDifficulty.jsx
@@ -5,8 +5,11 @@ export default function ChooseDifficulty(props) {
 
     return (
         <div className="flex" onChange={props.handleDifficulty}>
+           <a href="#" className="no-underline group relative inline-block text-blue-500 hover:text-red-500 duration-300">
+
           <div className="flex items-center mr-4">
-            <input  
+
+          <input  
               defaultChecked={props.difficulty == 10} 
               id="inline-radio" 
               type="radio" 
@@ -14,10 +17,17 @@ export default function ChooseDifficulty(props) {
               name="inline-radio-group" 
               className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800  dark:bg-gray-700 dark:border-gray-600"
             />
+            <span
+                class="absolute hidden group-hover:flex -left-5 -top-2 -translate-y-full w-32 px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700">
+                10 seconds to answer</span>
+      
+            
             <label htmlFor="inline-radio" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Easy</label>
           </div>
+          </a>
+          <a href="#" class="no group relative inline-block text-blue-500 no-underline hover:text-red-500 duration-300">
           <div className="flex items-center mr-4">
-            <input 
+          <input 
               defaultChecked={props.difficulty == 6} 
               id="inline-2-radio" 
               type="radio" 
@@ -25,8 +35,14 @@ export default function ChooseDifficulty(props) {
               name="inline-radio-group" 
               className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300  dark:ring-offset-gray-800  dark:bg-gray-700 dark:border-gray-600"
             />
+            <span
+                class="absolute hidden group-hover:flex -left-5 -top-2 -translate-y-full w-32 px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700">
+                6 seconds to answer</span>
+            
             <label htmlFor="inline-2-radio" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Medium</label>
           </div>
+        </a>
+        <a href="#" class="no group relative inline-block text-blue-500 no-underline hover:text-red-500 duration-300">
           <div className="flex items-center mr-4">
             <input 
               defaultChecked={props.difficulty == 3} 
@@ -36,8 +52,12 @@ export default function ChooseDifficulty(props) {
               name="inline-radio-group" 
               className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600"
             />
+            <span
+                class="absolute hidden group-hover:flex -left-5 -top-2 -translate-y-full w-32 px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm after:content-[''] after:absolute after:left-1/2 after:top-[100%] after:-translate-x-1/2 after:border-8 after:border-x-transparent after:border-b-transparent after:border-t-gray-700">
+                3 seconds to answer</span>
             <label htmlFor="inline-3-radio" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Hard</label>
           </div>
+          </a>
         </div>
 
     )
