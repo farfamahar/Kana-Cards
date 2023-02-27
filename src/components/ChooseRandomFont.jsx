7c5316a7091880384e0f2764dceaceeaab509cce
Farfama <farfama.hargaaya@gmail.com>
2023-02-27 22:19:41 +0900

    Update ChooseQuizMode, ChooseRandomFont
    -Prettier formatting
    -PropTypes added

--- src/components/ChooseRandomFont.jsx
+++ src/components/ChooseRandomFont.jsx
@@ -1,19 +1,27 @@
-import React from "react"
+import React from "react";
+import PropTypes from "prop-types";
 
 //Renders the checkbox to determine if user wants to take quiz with randomized fonts
-export default function ChooseRandomFont(props){
-
-    return (
-        <div className="flex items-center mb-4 mt-4">
-            <input 
-                id="default-checkbox" 
-                type="checkbox" 
-                onChange={props.handleRandomFont} 
-                value="" 
-                className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500"
-            />
-            <label htmlFor="default-checkbox" className="ml-2 text-sm font-medium text-gray-900">Randomize font</label>
-        </div>
-    )
-    
+export default function ChooseRandomFont({ handleRandomFont }) {
+  return (
+    <div className="flex items-center mb-4 mt-4">
+      <input
+        id="default-checkbox"
+        type="checkbox"
+        onChange={handleRandomFont}
+        value=""
+        className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500"
+      />
+      <label
+        htmlFor="default-checkbox"
+        className="ml-2 text-sm font-medium text-gray-900"
+      >
+        Randomize font
+      </label>
+    </div>
+  );
 }
+
+ChooseRandomFont.propTypes = {
+  handleRandomFont: PropTypes.func.isRequired,
+};
