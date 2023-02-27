29e43aa4d757bc31b8a1196e5b9ca168271a4040
Farfama <farfama.hargaaya@gmail.com>
2023-02-27 22:14:17 +0900

    Update ChooseLearnMode.jsx

--- src/components/ChooseLearnMode.jsx
+++ src/components/ChooseLearnMode.jsx
@@ -1,13 +1,33 @@
-import React from "react"
+import React from "react";
+import PropTypes from "prop-types";
+
 
 //Renders two buttons choose which type of learn mode to take (Hiragana or Katakana)
-export default function ChooseLearnMode(props){
-
-    return (
-        <div className='mb-5 w-max'> 
-            <h1 className='text-1xl font-bold uppercase mt-5 mb-3 text-black' > Learn</h1>
-            <button className="transition ease-in-out delay-50 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-200 ... bg-blue-500 text-white font-bold py-2 px-4 rounded" onClick={() => props.handleLearnChoice('hiragana')}> Hiragana</button>
-            <button className="transition ease-in-out delay-50 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-200 ... bg-blue-500 text-white font-bold py-2 px-4 rounded m-4" onClick={() => props.handleLearnChoice('katakana')}> Katakana</button>
-      </div>
-    )
+export default function ChooseLearnMode({handleLearnChoice}) {
+  return (
+    <div className="mb-5 w-max">
+      <h1 className="text-1xl font-bold uppercase mt-5 mb-3 text-black">
+        {" "}
+        Learn
+      </h1>
+      <button
+        className="transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-200 ... bg-blue-500 text-white font-bold py-2 px-4 rounded"
+        onClick={() => handleLearnChoice("hiragana")}
+      >
+        {" "}
+        Hiragana
+      </button>
+      <button
+        className="transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-200 ... bg-blue-500 text-white font-bold py-2 px-4 rounded m-4"
+        onClick={() => handleLearnChoice("katakana")}
+      >
+        {" "}
+        Katakana
+      </button>
+    </div>
+  );
 }
+
+ChooseLearnMode.propTypes = {
+    handleLearnChoice: PropTypes.func.isRequired,
+  };
