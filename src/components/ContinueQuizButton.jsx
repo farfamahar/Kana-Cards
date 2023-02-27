44a1cda57b5a1466e7982ff2a868d9f1b4cc52aa
Farfama <farfama.hargaaya@gmail.com>
2023-02-27 22:22:06 +0900

    Update ContinueQuizButton.jsx
    -Prettier formatting
    -PropTypes

--- src/components/ContinueQuizButton.jsx
+++ src/components/ContinueQuizButton.jsx
@@ -1,11 +1,18 @@
-import React from "react"
+import React from "react";
+import PropTypes from "prop-types";
 
 //Renders the button that appears when a wrong answer is submitted
-export default function ContinueQuizButton(props){
-    return(
-        <button 
-                className='transition ease-in-out delay-50 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-200 ... bg-blue-500 text-white font-bold py-2 px-4 rounded m-4' 
-                onClick={props.handlePause}>Continue
-        </button>
-    )
+export default function ContinueQuizButton({ handlePause }) {
+  return (
+    <button
+      className="transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-200 ... bg-blue-500 text-white font-bold py-2 px-4 rounded m-4"
+      onClick={handlePause}
+    >
+      Continue
+    </button>
+  );
 }
+
+ContinueQuizButton.propTypes = {
+  handlePause: PropTypes.func.isRequired,
+};
