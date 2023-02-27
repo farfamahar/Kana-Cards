50aec44338cfbf6d1b1dfef85152cfc6b9f14a32
Farfama <farfama.hargaaya@gmail.com>
2023-02-27 22:36:21 +0900

    Update Credits, KanaMnemonic, KanaLearn
    -Prettier formatting
    -PropTypes

--- src/KanaLearn.jsx
+++ src/KanaLearn.jsx
@@ -1,67 +1,62 @@
-import { useState, useEffect } from 'react'
-import LearnModeHeader from './components/LearnModeHeader'
-import KanaMnemonic from './components/KanaMnemonic'
-import NextKanaButton from './components/NextKanaButton'
-import useLearnMode from './hooks/useLearnMode'
-import PlaySoundButton from './components/PlaySoundButton'
-import ReturnHome from './components/ReturnHome'
-
-
+import React, { useEffect } from "react";
+import LearnModeHeader from "./components/LearnModeHeader";
+import KanaMnemonic from "./components/KanaMnemonic";
+import NextKanaButton from "./components/NextKanaButton";
+import useLearnMode from "./hooks/useLearnMode";
+import PlaySoundButton from "./components/PlaySoundButton";
+import ReturnHome from "./components/ReturnHome";
 
 function KanaLearn(props) {
+  const {
+    volumeIcon,
+    current,
+    iterator,
+    kana,
+    setNextKana,
+    setPrevKana,
+    resetQuiz,
+    playSound,
+  } = useLearnMode(props);
 
+  //play new sound on next kana
+  useEffect(() => {
+    if (iterator < kana.length + 1) {
+      playSound();
+    }
+  }, [iterator]);
 
-  const { volumeIcon,
-          current,
-          iterator,
-          kana,
-          setNextKana,
-          setPrevKana,
-          resetQuiz,
-          playSound } = useLearnMode(props);
-
-
-    //play new sound on next kana
-    useEffect(() => {
-      if(iterator < kana.length + 1){
-        playSound()
-      }
-    },[iterator])
-
-return (
-  <div className= "min-h-screen centerFlex bg-slate-50" > 
-    {iterator < kana.length + 1 ? 
-    <div className='flex justify-center  bg-slate-50 text-black text-center scale-75'>
-      <div data-aos="slide-up" className=" m-10 p-10 max-w-md rounded shadow-lg bg-white card card-top-right soft-shadow" >
-        <div className="card-inner ml-4">
-          <LearnModeHeader
-            kana={kana}
-            current={current}
-          />
-          <div>
-            <PlaySoundButton
-              playSound={playSound}
-              volumeIcon={volumeIcon}
-            />
+  return (
+    <div className="min-h-screen centerFlex bg-slate-50">
+      {iterator < kana.length + 1 ? (
+        <div className="flex justify-center  bg-slate-50 text-black text-center scale-75">
+          <div
+            data-aos="slide-up"
+            className=" m-10 p-10 max-w-md rounded shadow-lg bg-white card card-top-right soft-shadow"
+          >
+            <div className="card-inner ml-4">
+              <LearnModeHeader kana={kana} current={current} />
+              <div>
+                <PlaySoundButton
+                  playSound={playSound}
+                  volumeIcon={volumeIcon}
+                />
+              </div>
+              <div>
+                <KanaMnemonic kana={kana} current={current} />
+                <NextKanaButton
+                  setNextKana={setNextKana}
+                  setPrevKana={setPrevKana}
+                />
+              </div>
+            </div>
           </div>
-          <div> 
-            <KanaMnemonic 
-              kana={kana} 
-              current={current}
-            />  
-            <NextKanaButton
-              setNextKana={setNextKana}
-              setPrevKana={setPrevKana}
-            /> 
-          </div>
-          
         </div>
-      </div>
-    </div> : resetQuiz()}
-    <ReturnHome/>
-  </div>
-  )
+      ) : (
+        resetQuiz()
+      )}
+      <ReturnHome />
+    </div>
+  );
 }
 
-
-export default KanaLearn
+export default KanaLearn;
--- src/components/KanaMnemonic.jsx
+++ src/components/KanaMnemonic.jsx
@@ -1,23 +1,37 @@
-import React,{useState} from "react"
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 
 //Renders the WaniKani Mnemonic image contained in the kana array
-export default function KanaMnemonic(props){
+export default function KanaMnemonic({ kana, current }) {
+  const [scaleUpImage, setScaleUpImage] = useState(false);
 
-    const [scaleUpImage, setScaleUpImage] = useState(false);
+  function handleScale() {
+    setScaleUpImage((prev) => !prev);
+  }
 
-    function handleScale(){
-        setScaleUpImage(prev=>!prev)
-    }
-
-    return(
-        <div className="text-3xl font-bold mb-2">
-            <img 
-                width="325px" 
-                height="335px" 
-                className={ scaleUpImage ? "transition scale-125" : "transition scale-75"}
-                src={props.kana[props.current].image}
-                onClick={handleScale}
-            />
-        </div>
-    )
+  return (
+    <div className="text-3xl font-bold mb-2">
+      <img
+        width="325px"
+        height="335px"
+        className={
+          scaleUpImage
+            ? "transition scale-125 cursor-pointer"
+            : "transition scale-75 cursor-pointer"
+        }
+        src={kana[current].image}
+        onClick={handleScale}
+      />
+    </div>
+  );
 }
+
+KanaMnemonic.propTypes = {
+  kana: PropTypes.shape({
+    romanji: PropTypes.string,
+    kana: PropTypes.string,
+    iamge: PropTypes.string,
+    audio: PropTypes.string,
+  }),
+  current: PropTypes.number.isRequired,
+};
