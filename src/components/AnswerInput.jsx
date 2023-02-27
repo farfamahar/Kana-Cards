8917a28fbc2e02813b980a099733d662ef5db3c5
Farfama <farfama.hargaaya@gmail.com>
2023-02-27 22:05:15 +0900

    Update AnswerInput PropTypes

--- src/components/AnswerInput.jsx
+++ src/components/AnswerInput.jsx
@@ -1,20 +1,31 @@
-import React from "react"
+import React from "react";
+import PropTypes from "prop-types";
 
 //Renders the answer form where the user submits a quiz answer
-export default function AnswerInput(props){
-
-    
-    return(
-        <form id="myform"  onSubmit={(event) => props.handleSubmit(event)} autoComplete="off">
-              {!props.pause && <input 
-                id="kanaInput"
-                autoFocus
-                type="text"
-                value={props.input}
-                onChange={props.handleChange}
-                className="block w-24 mx-auto pb-2 bg-transparent border-b-2 border-b-black outline-none
-              text-center text-2xl "/>}
-              {/* {props.isCorrect && <input 
+export default function AnswerInput({
+  pause,
+  input,
+  handleSubmit,
+  handleChange,
+}) {
+  return (
+    <form
+      id="myform"
+      onSubmit={(event) => handleSubmit(event)}
+      autoComplete="off"
+    >
+      {!pause && (
+        <input
+          id="kanaInput"
+          autoFocus
+          type="text"
+          value={input}
+          onChange={handleChange}
+          className="block w-24 mx-auto pb-2 bg-transparent border-b-2 border-b-black outline-none
+              text-center text-2xl "
+        />
+      )}
+      {/* {props.isCorrect && <input 
                 id="kanaInput"
                 disabled
                 autoFocus
@@ -23,7 +34,14 @@ export default function AnswerInput(props){
                 onChange={props.handleChange}
                 className="block w-24 mx-auto pb-2 bg-transparent border-b-2 border-green-500 outline-none
               text-center text-2xl text-green-500 "/>} */}
-              <button id="submitForm"></button>
-        </form>
-    )
+      <button id="submitForm"></button>
+    </form>
+  );
 }
+
+AnswerInput.propTypes = {
+  input: PropTypes.string.isRequired,
+  pause: PropTypes.bool.isRequired,
+  handleSubmit: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired
+};
