73ddc9393972477dd2d52e5c591b14876f7e4461
Farfama <farfama.hargaaya@gmail.com>
2023-02-27 23:01:09 +0900

    Update ErrorMessage.jsx
    -Prettier formatting
    -Destructure props
    -PropTypes

--- src/components/ErrorMessage.jsx
+++ src/components/ErrorMessage.jsx
@@ -1,8 +1,16 @@
-import React from "react"
+import React from "react";
+import PropTypes from "prop-types";
 
 //Renders an error display the correct answer when answering wrong
-export default function ErrorMessage(props){
-    return(
-        <p data-aos="fade-up" className="text-red-600 , text-center "> {props.error} </p>
-    )
+export default function ErrorMessage({ error }) {
+  return (
+    <p data-aos="fade-up" className="text-red-600 , text-center ">
+      {" "}
+      {error}{" "}
+    </p>
+  );
 }
+
+ErrorMessage.propTypes = {
+  error: PropTypes.bool.isRequired,
+};
