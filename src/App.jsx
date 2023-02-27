6993888a2fe169894fb9d78f9efcc8848895c1e3
Farfama <farfama.hargaaya@gmail.com>
2023-02-27 22:09:11 +0900

    Update ChooseDakutan PropTypes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import KanaQuiz from './KanaQuiz';
-import KanaLearn from './KanaLearn';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-import TitleHeader from "./components/TitleHeader"
-import useMenu from './hooks/useMenu';
-import ChooseDifficulty from './components/ChooseDifficulty';
-import ChooseDakutan from './components/ChooseDakutan';
-import ChooseRandomFont from './components/ChooseRandomFont';
-import ChooseLearnMode from "./components/ChooseLearnMode"
-import ChooseQuizMode from "./components/ChooseQuizMode"
-import Credits from "./components/Credits"
+import React from "react";
+import KanaQuiz from "./KanaQuiz";
+import KanaLearn from "./KanaLearn";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import TitleHeader from "./components/TitleHeader";
+import useMenu from "./hooks/useMenu";
+import ChooseDifficulty from "./components/ChooseDifficulty";
+import ChooseDakutan from "./components/ChooseDakutan";
+import ChooseRandomFont from "./components/ChooseRandomFont";
+import ChooseLearnMode from "./components/ChooseLearnMode";
+import ChooseQuizMode from "./components/ChooseQuizMode";
+import Credits from "./components/Credits";
 
 AOS.init();
 
-
 function App() {
-
-  const { quiz,
+  const {
+    quiz,
     learn,
     show,
     difficulty,
@@ -26,62 +26,58 @@ function App() {
     handleDakutan,
     handleRandomFont,
     handleQuizChoice,
-    handleLearnChoice} = useMenu();
-  
+    handleLearnChoice,
+  } = useMenu();
+
   return (
     <div>
-    {show &&
-      <div data-aos="slide-up"  className=" min-h-screen  centerFlex  mobile-card">
-        <div className=" text-white max-w-lg rounded-lg overflow-hidden shadow-lg bg-white soft-shadow pr-16 pl-16">
-      
-       <TitleHeader/>
-       <ChooseLearnMode handleLearnChoice={handleLearnChoice}/>
+      {show && (
+        <div
+          data-aos="slide-up"
+          className=" min-h-screen  centerFlex  mobile-card"
+        >
+          <div className=" text-white max-w-lg rounded-lg overflow-hidden shadow-lg bg-white soft-shadow pr-16 pl-16">
+            <TitleHeader />
+            <ChooseLearnMode handleLearnChoice={handleLearnChoice} />
 
-        <hr className='mt-3 mb-3'/>
+            <hr className="mt-3 mb-3" />
 
-        <h1 className='text-1xl font-bold uppercase mt-5 mb-3 text-black	' > Quiz</h1> 
-        <ChooseDifficulty 
-          handleDifficulty={handleDifficulty} 
+            <h1 className="text-1xl font-bold uppercase mt-5 mb-3 text-black	">
+              {" "}
+              Quiz
+            </h1>
+            <ChooseDifficulty
+              handleDifficulty={handleDifficulty}
+              difficulty={difficulty}
+            />
+            <p className="text-slate-400 text-xs mt-2">
+              {" "}
+              {difficulty} seconds to answer
+            </p>
+
+            <ChooseDakutan handleDakutan={handleDakutan} />
+
+            <ChooseRandomFont handleRandomFont={handleRandomFont} />
+
+            <ChooseQuizMode handleQuizChoice={handleQuizChoice} />
+            <Credits />
+          </div>
+        </div>
+      )}
+
+      {quiz && (
+        <KanaQuiz
+          quiz={quiz}
           difficulty={difficulty}
+          dakutan={isDakutan}
+          randomFont={isRandomFont}
         />
-        <p className="text-slate-400 text-xs mt-2"> {difficulty} seconds to answer</p>
-        
-        <ChooseDakutan
-          handleDakutan={handleDakutan}
-        />
+      )}
+      {learn && <KanaLearn quiz={learn} />}
 
-        <ChooseRandomFont
-          handleRandomFont={handleRandomFont}
-        />
+      {show && <div className="bg-scroll"> </div>}
+    </div>
+  );
+}
 
-        <ChooseQuizMode 
-          handleQuizChoice={handleQuizChoice}
-        />
-              <Credits/>
-
-      </div>
-
-
-    </div>}
-
-      {quiz && <KanaQuiz 
-                  quiz={quiz} 
-                  difficulty={difficulty} 
-                  dakutan={isDakutan} 
-                  randomFont={isRandomFont} 
-                />
-      }
-      {learn && <KanaLearn 
-                  quiz={learn} 
-                />
-      }
-
-      {show && <div className='bg-scroll'> </div>}
-
-
-    </div> 
-    )
-  }
-
-
-export default App
+export default App;
--- src/components/ChooseDakutan.jsx
+++ src/components/ChooseDakutan.jsx
@@ -1,18 +1,27 @@
-import React from "react"
+import React from "react";
+import PropTypes from "prop-types";
 
 //Renders checkbox to determine if quiz should contain modified kana
-export default function ChooseDakutan(props){
-    
-    return (
-        <div className="flex items-center mb-4 mt-4">
-          <input 
-            id="default-checkbox" 
-            type="checkbox" 
-            onChange={props.handleDakutan} 
-            value="" 
-            className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500"
-          />
-          <label htmlFor="default-checkbox" className="ml-2 text-sm font-medium text-gray-900">Include dakuten and handakuon</label>
-        </div>
-    )
+export default function ChooseDakutan({ handleDakutan }) {
+  return (
+    <div className="flex items-center mb-4 mt-4">
+      <input
+        id="default-checkbox"
+        type="checkbox"
+        onChange={handleDakutan}
+        value=""
+        className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500"
+      />
+      <label
+        htmlFor="default-checkbox"
+        className="ml-2 text-sm font-medium text-gray-900"
+      >
+        Include dakuten and handakuon
+      </label>
+    </div>
+  );
 }
+
+ChooseDakutan.propTypes = {
+  handleDakutan: PropTypes.func.isRequired,
+};
