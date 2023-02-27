6e0d5b91532a564d85756dad08a76a3d07ff486c
Farfama <farfama.hargaaya@gmail.com>
2023-02-27 22:22:33 +0900

    Update Credits.jsx
    -Prettier formatting

--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import CreditsModal from './CreditsModal';
+import CreditsModal from "./CreditsModal";
 
 export default function Credits() {
   const [showModal, setShowModal] = React.useState(false);
@@ -7,16 +7,14 @@ export default function Credits() {
   return (
     <div>
       <button
-        className='text-slate-400 absolute bottom-0 right-0 h-16 w-16'
+        className="text-slate-400 absolute bottom-0 right-0 h-16 w-16"
         type="button"
         data-testid="credits-modal-btn"
         onClick={() => setShowModal(true)}
       >
         ?
       </button>
-      {showModal ? (
-        <CreditsModal closeModal={setShowModal} />
-      ) : null}
+      {showModal ? <CreditsModal closeModal={setShowModal} /> : null}
     </div>
   );
 }
